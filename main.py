1b1a71469d96155c07f756f424f772dc0024113d
Andy Osborne <cao89@arizona.edu>
2025-07-03 08:43:05 -0700

    Fix address already in use error and allow for graceful exist with ctrl+c

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from http_request import HTTPRequest
 from http_response import HTTPResponse
 import datetime
 import mimetypes
+import sys
 
 def open_conn():
     HOST = ""
@@ -10,8 +11,10 @@ def open_conn():
     WEB_ROOT = "./www"
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        # setsockopt must be called before s.bind
+        # socket.SO_REUSEADDR will keep the address and port free  after the program exits
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST, PORT))
         s.listen(1)
         conn, addr = s.accept()
         with conn:
@@ -24,8 +27,14 @@ def open_conn():
 
 def main():
     print("Listening on localhost:8080...")
+    # Without this while loop, the socket will close when the use closes their browser
+    # and the program will end.
     while True:
-        open_conn()
+        try:
+            open_conn()
+        except KeyboardInterrupt:
+            break
+    sys.exit()
 
 if __name__ == "__main__":
     main()
