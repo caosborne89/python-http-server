718f6b6a43d36511c2ada9bea6274347134dfa60
Andy Osborne <cao89@arizona.edu>
2025-07-26 08:21:47 -0700

    Add multi-connection functionality

--- main.py
+++ main.py
@@ -4,37 +4,51 @@ from http_response import HTTPResponse
 import datetime
 import mimetypes
 import sys
+import selectors
+
 
 HOST = ""
 PORT = 8080
 WEB_ROOT = "./www"
 
+
 def open_conn():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # setsockopt must be called before s.bind
-        # socket.SO_REUSEADDR will keep the address and port free  after the program exits
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
-        s.listen(1)
+    sel = selectors.DefaultSelector()
+
+    def accept(sock, mask):
         conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                req = HTTPRequest(data.decode("utf-8"))
-                conn.sendall(HTTPResponse(WEB_ROOT, req.get_path()).get_raw_response())
+        conn.setblocking(False)
+        sel.register(conn, selectors.EVENT_READ, read)
+
+    def read(conn, mask):
+        data = conn.recv(1024)
+        if data:
+            req = HTTPRequest(data.decode("utf-8"))
+            conn.send(HTTPResponse(WEB_ROOT, req.get_path()).get_raw_response())
+        else:
+            sel.unregister(conn)
+            conn.close()
+
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    s.setblocking(False)
+    sel.register(s, selectors.EVENT_READ, accept)
+
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
 
 def main():
     print(f"Listening on {HOST if HOST else "localhost"}:{PORT}...")
-    # Without this while loop, the socket will close when the use closes their browser
-    # and the program will end.
-    while True:
-        try:
-            open_conn()
-        except KeyboardInterrupt:
-            break
-    sys.exit()
+
+    try:
+        open_conn()
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == "__main__":
     main()
